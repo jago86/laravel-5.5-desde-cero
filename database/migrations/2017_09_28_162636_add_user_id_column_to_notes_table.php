845c54507d9483c5f1a7687150581c8de7d5cee0
Jairo Ushiña <jago86@gmail.com>
2017-10-04 17:32:20 -0500

    Migraciones II

---
+++ database/migrations/2017_09_28_162636_add_user_id_column_to_notes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserIdColumnToNotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('notes', function (Blueprint $table) {
+            $table->integer('user_id')->unsigned()->after('id');
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+}
