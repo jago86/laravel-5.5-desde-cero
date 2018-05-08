609a3ad3f2707ec87660341c0ec9e48f18580c01
Jairo Ushiña <jago86@gmail.com>
2018-05-08 15:47:34 -0500

    25.1 Formularios auth a español

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,8 +34,8 @@
           <ul class="nav navbar-nav navbar-right">
             <!-- Authentication Links -->
             @guest
-                <li><a href="{{ route('login') }}">Login</a></li>
-                <li><a href="{{ route('register') }}">Register</a></li>
+                <li><a href="{{ route('login') }}">Iniciar sesión</a></li>
+                <li><a href="{{ route('register') }}">Registrarse</a></li>
             @else
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
@@ -47,7 +47,7 @@
                             <a href="{{ route('logout') }}"
                                 onclick="event.preventDefault();
                                          document.getElementById('logout-form').submit();">
-                                Logout
+                                Cerrar sesión
                             </a>
 
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
